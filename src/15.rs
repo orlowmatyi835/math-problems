7ca4dc7f8a97423966d116a9512e86dcc32c60f7
orlowmatyi835 <orlowmatyi835@gmail.com>
2025-03-22 15:43:34 +0000

    Add 15.rs

---
+++ src/15.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // Example Rust code for solving simple arithmetic and logical operations
+
+    let number1: i32 = 5;
+    let number2: i32 = 3;
+    let result: i32 = number1 + number2;
+    
+    println!("The sum of {} and {} is {}", number1, number2, result);
+}
