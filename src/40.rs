b3a52927fd66dd1c1ce5f5529923f0ca28086a41
orlowmatyi835 <orlowmatyi835@gmail.com>
2025-05-27 05:56:17 +0000

    Add 40.rs

---
+++ src/40.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // Example Rust code to demonstrate basic mathematical operations.
+    
+    let number1 = 5;
+    let number2 = 3;
+
+    let sum = number1 + number2;
+    let product = number1 * number2;
+    let difference = number1 - number2;
+    let quotient = number1 / number2;
+    let remainder = number1 % number2;
+
+    println!("The sum of {} and {} is {}", number1, number2, sum);
+    println!("The product of {} and {} is {}", number1, number2, product);
+    println!("The difference between {} and {} is {}", number1, number2, difference);
+    println!("The quotient when dividing {} by {} is {}", number1, number2, quotient);
+    println!("The remainder when dividing {} by {} is {}", number1, number2, remainder);
+}
